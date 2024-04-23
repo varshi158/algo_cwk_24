ec919da6b2181208802b592ab715f1bba423cd47
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2024-04-23 12:23:52 +0530

    update welcome message and add error handling to validate user input

--- src/PuzzleMain.java
+++ src/PuzzleMain.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class PuzzleMain {
     public static void main(String[] args) {
-        System.out.println("==============================================");
-        System.out.println("--------Welcome to Sliding Puzzle Game-------");
-        System.out.println("==============================================\n\n");
+        System.out.println(
+                """
+                        =============================================
+                        |     WELCOME TO THE SLIDING PUZZLE GAME    |
+                        =============================================
+                """);
 
         // Scanner class to read user input
         Scanner myScanner = new Scanner(System.in);
@@ -13,20 +16,33 @@ public class PuzzleMain {
 
         while (true) {
             // options for user to select from
-            System.out.println("Choose an option below:");
+            System.out.println("Select from an option below:");
             System.out.println("1: To load a puzzle from an existing file.");
             System.out.println("2: To select from an available puzzle.");
             System.out.println("3. To exit.");
+
+            try {
+                userChoice = myScanner.nextInt();
+
+                if (userChoice < 1 || userChoice > 3)
+                {
+                    throw new IllegalArgumentException("Invalid choice. Please enter a number between 1 and 3.");
+                }
+                else {
+                    /*
+                     break out of the loop
+                     if user input is valid
+                    */
+                    break;
+                }
+            }
+            catch (Exception e) {
+                // handle invalid input
+                System.out.println("Invalid input. Please enter a number between 1 and 3.");
+                myScanner.nextLine();
+            }
         }
-
-
-
-
-
-
-
-
-
+        System.out.println("You chose option: " + userChoice);
 
     }
 }
