1861c189f6af59990b870af9b5339b432c244410
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2024-04-23 12:54:29 +0530

    add switch statement to handle user choice

--- src/PuzzleMain.java
+++ src/PuzzleMain.java
@@ -5,16 +5,17 @@ public class PuzzleMain {
     public static void main(String[] args) {
         System.out.println(
                 """
-                        =============================================
-                        |     WELCOME TO THE SLIDING PUZZLE GAME    |
-                        =============================================
+                =============================================
+                |     WELCOME TO THE SLIDING PUZZLE GAME    |
+                =============================================
                 """);
 
         // Scanner class to read user input
         Scanner myScanner = new Scanner(System.in);
 
-        int userChoice = 0;
+        int userChoice;
 
+        /* keeps running until valid input is entered */
         while (true) {
             // options for user to select from
             System.out.println("Select from an option below:");
@@ -27,20 +28,36 @@ public class PuzzleMain {
 
                 if (userChoice < 1 || userChoice > 3)
                 {
-                    System.out.println("Invalid input. Please enter a number between 1 and 3.");
+                    System.out.println("\nInvalid input. " +
+                            "Please enter a number between 1 and 3.");
                 }
                 else {
                     /* break out of the loop
-                    only if user input is valid*/
+                    only if user input is valid */
                     break;
                 }
             }
             catch (InputMismatchException e) {
                 // handle invalid input
-                System.out.println("Invalid input. Please enter a number between 1 and 3.");
+                System.out.println("\nInvalid input. " +
+                        "Please enter a number between 1 and 3.");
                 myScanner.nextLine();
             }
+        } // end of while loop - user input
+
+        // enhanced switch statement to handle user choice
+        switch (userChoice) {
+            // load puzzle from file
+            case 1 -> System.out.println(" ");
+
+            // select puzzle to load
+            case 2 -> System.out.println(" ");
+
+            // exit from the program
+            case 3 -> System.out.println("Exiting from the game..." +
+                    "\nSee you soon!");
         }
 
-    }
+
+    } // end of main method
 }
