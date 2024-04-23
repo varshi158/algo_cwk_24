ea676170f280a61d8de324a1ae7e762b6ed804ba
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2024-04-23 11:43:18 +0530

    add options for user to select from

--- src/PuzzleMain.java
+++ src/PuzzleMain.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 public class PuzzleMain {
     public static void main(String[] args) {
         System.out.println("==============================================");
         System.out.println("--------Welcome to Sliding Puzzle Game-------");
-        System.out.println("==============================================");
+        System.out.println("==============================================\n\n");
+
+        // Scanner class to read user input
+        Scanner myScanner = new Scanner(System.in);
+
+        int userChoice = 0;
+
+        while (true) {
+            // options for user to select from
+            System.out.println("Choose an option below:");
+            System.out.println("1: To load a puzzle from an existing file.");
+            System.out.println("2: To select from an available puzzle.");
+            System.out.println("3. To exit.");
+        }
+
+
+
+
+
+
+
+
 
 
     }
