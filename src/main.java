5a9af06bb93e5c809389189d2b349422353d1157
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2024-04-23 10:56:33 +0530

    add welcome message

--- src/main.java
+++ src/main.java
@@ -1,3 +1,9 @@
 public class main {
+    public static void main(String[] args) {
+        System.out.println("==============================================");
+        System.out.println("--------Welcome to Sliding Puzzle Game-------");
+        System.out.println("==============================================");
 
+
+    }
 }
