25f8f5aa25cddfc3c77a4b39e46b5032c8baca6b
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2024-04-24 18:34:07 +0530

    initialise visited node array, unvisited node array and the start node

--- src/PuzzleSolver.java
+++ src/PuzzleSolver.java
@@ -1,6 +1,23 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class PuzzleSolver {
 
+    // method to solve the puzzle using bfs
     public static void bfsPathFinder(char [][] puzzle, int startPointRow, int startPointCol, int finishPointRow, int finishPointCol) {
+
+        // initialise array to keep track of visited nodes
+        boolean [][] visitedNodes = new boolean[puzzle.length][puzzle[0].length];
+
+        // initialise queue for bfs
+        Queue<Node> queue = new LinkedList<>();
+
+        // initialise start node
+        Node startNode = new Node(startPointRow, startPointCol, null);
+
+        // add the start node to the queue
+        queue.offer(startNode);
+
 
     }
 }
