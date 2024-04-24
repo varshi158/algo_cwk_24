f0d2d788e1b8c46b58036027d918fab52e531e89
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2024-04-24 10:51:47 +0530

    update Node class to represent an element in the puzzle

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,18 @@
 public class Node {
+
+    // row and column indices of the node
+    int row;
+    int col;
+
+    // parent node of the current node
+    Node parent;
+
+    // construct a new node with its row index,
+    // column index, and parent node.
+    public Node(int row, int col, Node parent) {
+        this.row = row;
+        this.col = col;
+        this.parent = parent;
+    }
+
 }
